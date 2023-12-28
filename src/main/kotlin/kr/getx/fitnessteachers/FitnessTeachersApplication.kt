a1c1a9b0d3f80eb958fb789cdbcf975ab041939b
brucedev <whdtmek@gmail.com>
2023-12-28 13:06:13 +0900

    init

---
+++ src/main/kotlin/kr/getx/fitnessteachers/FitnessTeachersApplication.kt
@@ -0,0 +1,11 @@
+package kr.getx.fitnessteachers
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FitnessTeachersApplication
+
+fun main(args: Array<String>) {
+    runApplication<FitnessTeachersApplication>(*args)
+}
---
+++ src/test/kotlin/kr/getx/fitnessteachers/FitnessTeachersApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.getx.fitnessteachers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FitnessTeachersApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
