0acad6abb4e133f5a7411837490ed530006acf8b
BigNo <yjh990219@gmail.com>
2024-01-05 17:29:46 +0900

    리디렉션 하고, 유저정보 추가할 때 소셜로그인으로 받아온 정보 DB에 저장

--- src/main/kotlin/kr/getx/fitnessteachers/controller/UserController.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/controller/UserController.kt
@@ -1,14 +1,14 @@
 package kr.getx.fitnessteachers.controller
 
 import kr.getx.fitnessteachers.common.response.CommonResult
-import kr.getx.fitnessteachers.common.response.SingleResult
 import kr.getx.fitnessteachers.common.service.ResponseService
 import kr.getx.fitnessteachers.entity.User
 import kr.getx.fitnessteachers.service.UserService
 import lombok.RequiredArgsConstructor
-import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
+import org.springframework.http.ResponseEntity
 
 @RestController
 @RequiredArgsConstructor
@@ -22,7 +22,17 @@ class UserController(private val userService: UserService) {
   fun getAllUsers(): List<User> = userService.getAllUsers()
 
   @PostMapping("/add")
-  fun addUser(@RequestBody user: User): User = userService.addUser(user)
+  fun addUserFromNaver(authentication: OAuth2AuthenticationToken): ResponseEntity<User> {
+    val naverUser = authentication.principal.attributes
+    val user = User(
+      User_Social_Media_ID = naverUser["id"] as String,
+      Email = naverUser["email"] as String,
+      Username = naverUser["name"] as String,
+      PhoneNumber = naverUser["mobile"] as String
+    )
+    val savedUser = userService.addUser(user)
+    return ResponseEntity.ok(savedUser)
+  }
 
   @GetMapping("/{id}")
   fun getUser(@PathVariable id: Int): CommonResult = responseService.getSingleResult(userService.getUserById(id))
