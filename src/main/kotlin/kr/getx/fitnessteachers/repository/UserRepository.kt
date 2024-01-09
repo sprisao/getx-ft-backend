5ebb134c0f3e85779ed7bf72967a9ef7735df8f3
BigNo <yjh990219@gmail.com>
2024-01-09 16:10:17 +0900

    백업

--- src/main/kotlin/kr/getx/fitnessteachers/repository/UserRepository.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/repository/UserRepository.kt
@@ -5,11 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
 interface UserRepository : JpaRepository<User, Int>{
-    // 최초 소셜 로그인 가입자인지 확인
-    // userSocialMediaId 를 검색을 통해서 이미 있는 사용자인지 확인
-    fun findByUserSocialMediaId(userSocialMediaId: String): User?
+    fun findByUserSocialMediaId(userSocialMediaId: String): Optional<User>
 
-    // 중복 이메일 체크
-    // 이미 생성된 사용자인지 처음 가입하는 사용자인지 확인
-    fun findByEmailAndProvider(email: String?, provider: String?): Optional<User?>?
 }
--- src/main/kotlin/kr/getx/fitnessteachers/security/SecurityConfig.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/security/SecurityConfig.kt
@@ -1,42 +1,24 @@
 package kr.getx.fitnessteachers.security
 
-import kr.getx.fitnessteachers.repository.UserRepository
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 
 @Configuration
-class SecurityConfig(
-        private val oAuthService: OAuthService,
-    ) {
-    private val allowedUrls = arrayOf("/login/oauth2/code/naver", "/oauth2/**", "/login/**", "/error", "/health")
+class SecurityConfig{
+    private val allowedUrls = arrayOf("/*", "api/*", "/api/users/add", "/api/users/*", "/api/**", "/login", "/users", "/health")
 
     @Bean
     fun filterChain(http: HttpSecurity) = http
         .csrf { it.disable() }
         .authorizeHttpRequests {
-            it.requestMatchers(*allowedUrls).permitAll()
+            it.requestMatchers(*allowedUrls).permitAll()    // 허용할 url 목록을 배열로 분리했다
                 .anyRequest().authenticated()
         }
         .oauth2Login {
-            it.successHandler(MyAuthenticationSuccessHandler)
-            it.defaultSuccessUrl("/api/users/logininfo")
-            it.failureUrl("/")
+            it.defaultSuccessUrl("/api/users/all")
         }
-        .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED) }
+        .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
         .build()!!
-
-    @Bean
-    fun corsConfigurationSource(): CorsConfigurationSource {
-        val configuration = CorsConfiguration()
-        configuration.addAllowedOrigin("http://localhost:8080")
-        configuration.addAllowedMethod("*")
-        configuration.addAllowedHeader("*")
-        configuration.allowCredentials = true
-        val source = UrlBasedCorsConfigurationSource()
-        source.registerCorsConfiguration("/**", configuration)
-        return source
-    }
 }
