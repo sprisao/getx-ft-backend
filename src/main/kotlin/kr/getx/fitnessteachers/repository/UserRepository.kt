1b5dd7134a6f026684119bdd5557ddc178d041f8
BigNo <yjh990219@gmail.com>
2024-01-15 11:45:31 +0900

    불필요한 부분 삭제

--- src/main/kotlin/kr/getx/fitnessteachers/repository/UserRepository.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/repository/UserRepository.kt
@@ -2,11 +2,8 @@ package kr.getx.fitnessteachers.repository
 
 import kr.getx.fitnessteachers.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
 
 interface UserRepository : JpaRepository<User, Int>{
-    fun findByUserSocialMediaId(userSocialMediaId: String): User?
-
     // 중복 이메일 검사 메소드
     fun findByEmail(email: String): User?
 
