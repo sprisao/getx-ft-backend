baa33b6d62cb76b8aad64e0800714e9d6042bc80
BigNo <yjh990219@gmail.com>
2024-01-15 11:46:00 +0900

    불필요한 부분 삭제

--- src/main/kotlin/kr/getx/fitnessteachers/repository/UserRepository.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/repository/UserRepository.kt
@@ -2,7 +2,6 @@ package kr.getx.fitnessteachers.repository
 
 import kr.getx.fitnessteachers.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
 
 interface UserRepository : JpaRepository<User, Int>{
     fun findByUserSocialMediaId(userSocialMediaId: String): User?
--- src/main/kotlin/kr/getx/fitnessteachers/service/UserService.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/service/UserService.kt
@@ -3,7 +3,6 @@ package kr.getx.fitnessteachers.service
 import kr.getx.fitnessteachers.entity.User
 import kr.getx.fitnessteachers.repository.UserRepository
 import org.springframework.stereotype.Service
-import java.util.*
 
 @Service
 class UserService(private val userRepository: UserRepository) {
@@ -11,21 +10,6 @@ class UserService(private val userRepository: UserRepository) {
     fun getAllUsers(): List<User> = userRepository.findAll()
 
     fun getUserById(id : Int): User? = userRepository.findById(id).orElse(null)
-
-    fun addOrUpdateUser(userSocialMediaId: String, userInfo: Map<String, Any>): User {
-        val existingUser = userRepository.findByUserSocialMediaId(userSocialMediaId)
-        val user = existingUser?.apply {
-            this.username = userInfo["name"] as String
-            this.email = userInfo["email"] as String
-            this.phoneNumber = userInfo["mobile"] as String
-        } ?: User(
-            userSocialMediaId = userSocialMediaId,
-            username = userInfo["name"] as String,
-            email = userInfo["email"] as String,
-            phoneNumber = userInfo["mobile"] as String
-        )
-        return userRepository.save(user)
-    }
 
     fun deleteUser(id: Int) = userRepository.deleteById(id)
 }
