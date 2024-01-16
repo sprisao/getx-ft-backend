ef8b599b6be205e9313ad08e32ba932a2860e574
BigNo <yjh990219@gmail.com>
2024-01-16 22:32:07 +0900

    CORS 설정

--- src/main/kotlin/kr/getx/fitnessteachers/security/SecurityConfig.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/security/SecurityConfig.kt
@@ -2,8 +2,12 @@ package kr.getx.fitnessteachers.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 class SecurityConfig {
@@ -11,6 +15,7 @@ class SecurityConfig {
     @Bean
     fun filterChain(http: HttpSecurity) = http
         .csrf { it.disable() }
+        .cors(Customizer.withDefaults())
         .authorizeHttpRequests {
             it.requestMatchers(*allowedUrls).permitAll()    // 허용할 url 목록을 배열로 분리했다
                 .anyRequest().authenticated()
@@ -18,4 +23,15 @@ class SecurityConfig {
         .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED) } // 세션을 사용할 수 있게 변경
         // OAuth2 인증 프로세스에 필요한 세션 정보를 유지하기 위해 필요
         .build()!!
+
+    @Bean
+    fun corsConfigurationSource() : CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("*")
+        configuration.allowedMethods = listOf("*")
+        configuration.allowedHeaders = listOf("*")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
 }
