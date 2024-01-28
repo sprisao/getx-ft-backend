935bb7770f4b2281833c49ec74bf1e5195e86cad
BigNo <yjh990219@gmail.com>
2024-01-28 15:22:42 +0900

    허용 URL추가

--- src/main/kotlin/kr/getx/fitnessteachers/security/SecurityConfig.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/security/SecurityConfig.kt
@@ -14,7 +14,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 class SecurityConfig {
-    private val allowedUrls = arrayOf("/*", "/api/users/all", "/api/users/login", "/api/users/*", "/api/**", "/login", "/users", "/health")
+    private val allowedUrls = arrayOf("/*", "/api/users/all", "/api/users/login", "/api/users/userTypeEdit", "/api/users/***", "/api/**", "/login", "/users", "/health")
     val jwtTokenFilter = JwtTokenFilter(JwtUtils())
     @Bean
     fun filterChain(http: HttpSecurity) = http
