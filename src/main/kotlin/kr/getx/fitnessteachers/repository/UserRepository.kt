cc80e997bdd2e986c6e401a3facc0a170c9faeac
BigNo <yjh990219@gmail.com>
2024-03-02 18:38:27 +0900

    불필요한 부분 삭제

--- src/main/kotlin/kr/getx/fitnessteachers/repository/UserRepository.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/repository/UserRepository.kt
@@ -2,7 +2,6 @@ package kr.getx.fitnessteachers.repository
 
 import kr.getx.fitnessteachers.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
 
 interface UserRepository : JpaRepository<User, Int>{
     // 중복 이메일 검사 메소드
