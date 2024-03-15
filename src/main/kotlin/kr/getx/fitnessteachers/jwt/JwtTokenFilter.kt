4c45506a56a82b9c74489bf8c25c9155255c691e
BigNo219 <103584561+BigNo219@users.noreply.github.com>
2024-03-15 16:01:29 +0900

    접근권한 수정, JWT-> doFilterInternal()수정

--- src/main/kotlin/kr/getx/fitnessteachers/jwt/JwtTokenFilter.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/jwt/JwtTokenFilter.kt
@@ -11,30 +11,35 @@ import org.springframework.security.core.context.SecurityContextHolder
 
 class JwtTokenFilter(private val jwtUtils: JwtUtils) : OncePerRequestFilter() {
 
+    private val publicEndpoint = arrayOf("/api/users/**", "/api/jobPosts/all", "/api/centers/all")
+
     override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
+        val requestURI = request.requestURI
+
+        // publicEndpoint에 대해서는 토큰 검증을 생략
+        if(publicEndpoint.any { requestURI.startsWith(it) }) {
+            filterChain.doFilter(request, response)
+            return
+        }
+
         val token = jwtUtils.resolveToken(request)
         if (token != null && jwtUtils.validateToken(token)) {
             // Jwt 토큰에서 소셜 로그인 정보 추출
             val userData = jwtUtils.getSocialLoginInfo(token)
-            if(userData != null) {
-                val name = userData.name ?: throw IllegalArgumentException("Name is missing in the token")
-                val email = userData.email ?: throw IllegalArgumentException("Email is missing in the token")
-                val socialType = userData.socialType ?: throw IllegalArgumentException("Social Type is missing in the token")
+            val userDto = UserDto(
+                name = userData.name,
+                email = userData.email,
+                socialType = userData.socialType
+            )
+            // 인증 정보 저장
+            val auth = UsernamePasswordAuthenticationToken(
+                userDto,
+                null,
+                listOf(SimpleGrantedAuthority("ROLE_USER")) // 권한 설정
+            )
+            SecurityContextHolder.getContext().authentication = auth
+            request.setAttribute("userData", userData)
 
-                val userDto = UserDto(
-                    name = name,
-                    email = email,
-                    socialType = socialType
-                )
-                // 인증 정보 저장
-                val auth = UsernamePasswordAuthenticationToken(
-                    userDto,
-                    null,
-                    listOf(SimpleGrantedAuthority("ROLE_USER")) // 권한 설정
-                )
-                SecurityContextHolder.getContext().authentication = auth
-                request.setAttribute("userData", userData)
-            }
             filterChain.doFilter(request, response)
         } else {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "토큰 값이 유효하지 않습니다.")
