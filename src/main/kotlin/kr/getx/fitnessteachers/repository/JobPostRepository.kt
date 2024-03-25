b40b5c70d9515f3c744b25ae29288190a4e5736a
BigNo219 <103584561+BigNo219@users.noreply.github.com>
2024-03-25 13:59:48 +0900

    search Repository @Query 삽입

--- src/main/kotlin/kr/getx/fitnessteachers/repository/JobPostRepository.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/repository/JobPostRepository.kt
@@ -5,10 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.Query
 
 @Repository
 interface JobPostRepository : JpaRepository<JobPost, Int> {
     fun findByCenterCenterId(centerId: Int): List<JobPost>
+
+    @Query("SELECT j FROM JobPost j WHERE (:recruitmentStatus IS NULL OR j.recruitmentStatus = :recruitmentStatus) AND (:jobCategory IS NULL OR j.jobCategory = :jobCategory) AND (:locationProvince IS NULL OR j.center.locationProvince = :locationProvince) AND (:locationCity IS NULL OR j.center.locationCity = :locationCity)")
+
     fun search(
         recruitmentStatus: String?,
         jobCategory: String?,
