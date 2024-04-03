ef5e725f71a35499ab97cb972ada3758098db09f
BigNo219 <103584561+BigNo219@users.noreply.github.com>
2024-04-03 12:16:36 +0900

    api/jobPostApplications/{jobPostId}/applicantResumes -> Response Data중에 createdAt의 데이터 값 <-> jobPostApplication.createdAt 값으로 변경

--- src/main/kotlin/kr/getx/fitnessteachers/service/JobPostApplicationService.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/service/JobPostApplicationService.kt
@@ -32,7 +32,7 @@ class JobPostApplicationService (
     fun getApplicantResumes(jobPostId: Int): List<ResumeDto> =
         jobPostApplicationRepository.findByJobPostJobPostId(jobPostId).mapNotNull {
             val resume = resumeService.getResumeByUserId(it.user.userId)
-            resumeService.toDto(resume)
+            resumeService.toDto(resume).copy(createdAt = it.createdAt)
         }
     fun getAppliedJobPosts(userId: Int): List<JobPostDto> =
         jobPostApplicationRepository.findByUserUserId(userId).map {
