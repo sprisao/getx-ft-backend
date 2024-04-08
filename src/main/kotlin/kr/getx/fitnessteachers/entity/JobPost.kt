6cbdfddde73025c2b528f1cc44f7c775cf9ec552
BigNo219 <103584561+BigNo219@users.noreply.github.com>
2024-04-08 17:07:04 +0900

    JobPost Entity 객체 Columns 데이터들 수정

--- src/main/kotlin/kr/getx/fitnessteachers/entity/JobPost.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/entity/JobPost.kt
@@ -17,45 +17,71 @@ data class JobPost(
     val center: Center,
 
     // 작성 전, 작성 완료
-    var isDisplayReady: Boolean? = false,
+    var isPostCompleted: Boolean = false,
 
     // 모집중, 모집마감
-    var recruitmentStatus: Boolean? = false,
+    var isRecruitmentOpen : Boolean = false,
 
+    // 모집 직종 ( 요갸, 필라테스, 트레이너 )
+    @ElementCollection
+    var jobCategory: List<String>,
+
+    // 근무 위치
+    var workLocation: String,
+
+    // 근무 시간
+    var workHours: String,
+
+    // 근무 요일
+    var workDays: String,
+
+    // 임금 유형 ( 시급, 월급 )
+    var employmentType: String,
+
+    // 기본급 유무 ( 있음, 없음 )
+    var hasBaseSalary: Boolean,
+
+    // 급여
+    var salaryRange : String,
+
+    // 경력 요건 ( 경력 N년 이상 )
+    var experienceLevel: Int,
+
+    // 제2외국어 가능 여부
+    var isSecondLanguageAvailable: Boolean = false,
+
+    // 전공 학위 필요 여부
+    var isMajorDegreeRequired: Boolean = false,
+
+    // 모집 인원
+    var numberOfPositions: Int,
+
+    // 자격 요건
     @Column(length = 10000)
-    var responsibilities: String? = null,
+    var qualifications: String,
 
-    var workLocation: String? = null,
-
-    var workHours: String? = null,
-
-    var workDays: String? = null,
-
-    var employmentType: String? = null,
-
-    var numberOfPositions: Int? = null,
-
-    var salary: String? = null,
-
+    // 우대 사항
     @Column(length = 10000)
-    var qualifications: String? = null,
+    var preferences: String,
 
-    var applicationPeriodStart: LocalDate? = null,
+    // 간단 소개글
+    @Column(length = 10000)
+    var details: String,
 
+    // 지원 기간 종료일
     var applicationPeriodEnd: LocalDate? = null,
 
-    var contactEmail: String? = null,
+    // 근무 시작일
+    var workStartDate: LocalDate?,
 
-    var contactPhone: String? = null,
+    // 문의 이메일
+    var contactEmail: String,
 
-    var contactPerson: String? = null,
+    // 문의 연락처
+    var contactPhone: String,
 
-    var title: String? = null,
-
-    @Column(length = 10000)
-    var details: String? = null,
-
-    var jobCategory: String? = null,
+    // 문의 담당자
+    var contactPerson: String,
 
     @CreationTimestamp
     val postedDate: LocalDateTime = LocalDateTime.now()
