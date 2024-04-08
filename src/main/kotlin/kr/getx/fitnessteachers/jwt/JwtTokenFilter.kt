f7e37c6c8413a0c02f6453c8c409a800cfc1ed52
BigNo219 <103584561+BigNo219@users.noreply.github.com>
2024-04-08 17:08:02 +0900

    불필요한 부분 삭제

--- src/main/kotlin/kr/getx/fitnessteachers/jwt/JwtTokenFilter.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/jwt/JwtTokenFilter.kt
@@ -4,7 +4,6 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import kr.getx.fitnessteachers.dto.UserDto
-import kr.getx.fitnessteachers.entity.TeacherType
 import org.springframework.web.filter.OncePerRequestFilter
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.authority.SimpleGrantedAuthority
