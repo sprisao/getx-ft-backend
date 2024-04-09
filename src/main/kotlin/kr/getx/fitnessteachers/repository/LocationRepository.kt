7d92588dea2fb98f220cce852fd8971f63008df4
BigNo219 <103584561+BigNo219@users.noreply.github.com>
2024-04-09 14:07:17 +0900

    Location Entity 및 관련 삭제

--- src/main/kotlin/kr/getx/fitnessteachers/repository/LocationRepository.kt
+++
@@ -1,8 +0,0 @@
-package kr.getx.fitnessteachers.repository
-
-import kr.getx.fitnessteachers.entity.Location
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface LocationRepository : JpaRepository<Location, Int>{
-    fun findCitiesByParent(parent: Location): List<Location>
-}
