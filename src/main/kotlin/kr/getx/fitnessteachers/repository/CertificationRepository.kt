f67ed6d2aa3723ec1ce22d928244f8535f1f533a
BigNo219 <103584561+BigNo219@users.noreply.github.com>
2024-04-15 11:49:20 +0900

    CertificationRepository 일괄 수정

--- src/main/kotlin/kr/getx/fitnessteachers/repository/CertificationRepository.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/repository/CertificationRepository.kt
@@ -1,15 +1,13 @@
 package kr.getx.fitnessteachers.repository
 
 import kr.getx.fitnessteachers.entity.Certification
-import kr.getx.fitnessteachers.entity.Resume
+import kr.getx.fitnessteachers.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface CertificationRepository : JpaRepository<Certification, Int> {
-    // 필요한 경우 사용자 정의 메서드를 추가
+    fun findByCertificationIdIn(certificationIds: List<Int>): List<Certification>
 
-    fun findByResumeResumeId(resumeId: Int): List<Certification>
-
-    fun deleteAllByResume(resume: Resume)
+    fun findByUser(user: User): List<Certification>
 }
