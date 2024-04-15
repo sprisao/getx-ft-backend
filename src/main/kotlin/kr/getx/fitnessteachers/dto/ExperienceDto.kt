a89b405cfd66ba501fe16e8caf0732a94acf9d06
BigNo219 <103584561+BigNo219@users.noreply.github.com>
2024-04-15 12:09:06 +0900

    ExperienceDto 불필요한 부분 삭제

--- src/main/kotlin/kr/getx/fitnessteachers/dto/ExperienceDto.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/dto/ExperienceDto.kt
@@ -7,24 +7,17 @@ import java.time.LocalDateTime
 
 data class ExperienceDto(
     val experienceId: Int? = null,
+    val userId: Int,
     val description: String,
     val startDate: LocalDate,
     val endDate: LocalDate,
     val createdAt: LocalDateTime? = null
 )
 {
-    fun toExperience(resume: Resume): Experience = Experience(
-        experienceId = this.experienceId ?: 0,
-        resume = resume,
-        description = this.description,
-        startDate = this.startDate,
-        endDate = this.endDate,
-        createdAt = this.createdAt ?: LocalDateTime.now()
-    )
-
     companion object {
         fun fromEntity(experience: Experience): ExperienceDto = ExperienceDto(
             experienceId = experience.experienceId,
+            userId = experience.user.userId,
             description = experience.description,
             startDate = experience.startDate,
             endDate = experience.endDate,
