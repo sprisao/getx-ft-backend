2443b2a60587ab36e8c823b6e63581c1463e3caa
BigNo219 <103584561+BigNo219@users.noreply.github.com>
2024-04-15 12:09:18 +0900

    ExperienceRepository 일괄 수정

--- src/main/kotlin/kr/getx/fitnessteachers/repository/ExperienceRepository.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/repository/ExperienceRepository.kt
@@ -1,14 +1,13 @@
 package kr.getx.fitnessteachers.repository
 
 import kr.getx.fitnessteachers.entity.Experience
-import kr.getx.fitnessteachers.entity.Resume
+import kr.getx.fitnessteachers.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface ExperienceRepository : JpaRepository<Experience, Int> {
-    // 필요한 경우 사용자 정의 메서드를 추가
-    fun findByResumeResumeId(resumeId: Int): List<Experience>
+    fun findByExperienceIdIn(experienceIds: List<Int>): List<Experience>
 
-    fun deleteAllByResume(resume: Resume)
+    fun findByUser(user: User): List<Experience>
 }
