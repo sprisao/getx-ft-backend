ae73cd256f56ceab4d6cdff24a595cbc31f5d462
BigNo219 <103584561+BigNo219@users.noreply.github.com>
2024-04-15 11:57:16 +0900

    EducationRepository 일괄 수정

--- src/main/kotlin/kr/getx/fitnessteachers/repository/EducationRepository.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/repository/EducationRepository.kt
@@ -1,14 +1,13 @@
 package kr.getx.fitnessteachers.repository
 
 import kr.getx.fitnessteachers.entity.Education
-import kr.getx.fitnessteachers.entity.Resume
+import kr.getx.fitnessteachers.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface EducationRepository : JpaRepository<Education, Int> {
-    // 필요한 경우 사용자 정의 메서드를 추가
-    fun findByResumeResumeId(resumeId: Int): List<Education>
+    fun findByEducationIdIn(educationIds: List<Int>): List<Education>
 
-    fun deleteAllByResume(resume: Resume)
+    fun findByUser(user: User): List<Education>
 }
