89fbc99c88de476832c82e05619a2dac39b8f0fc
BigNo219 <103584561+BigNo219@users.noreply.github.com>
2024-04-18 12:35:06 +0900

    findByUser... > findByResume... 수정

--- src/main/kotlin/kr/getx/fitnessteachers/repository/JobPostApplicationRepository.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/repository/JobPostApplicationRepository.kt
@@ -4,7 +4,7 @@ import kr.getx.fitnessteachers.entity.JobPostApplication
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface JobPostApplicationRepository : JpaRepository<JobPostApplication, Int> {
-    fun findByUserUserIdAndJobPostJobPostId(userId: Int, jobPostId: Int): JobPostApplication?
+    fun findByResumeResumeIdAndJobPostJobPostId(resumeId: Int, jobPostId: Int): JobPostApplication?
     fun findByJobPostJobPostId(jobPostId: Int): List<JobPostApplication>
     fun findByUserUserId(userId: Int): List<JobPostApplication>
     fun countByJobPostJobPostId(jobPostId: Int): Int
