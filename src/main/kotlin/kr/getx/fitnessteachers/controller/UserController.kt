0a09316f056fa14c73c1a3fa8e160a5a727de767
BigNo219 <103584561+BigNo219@users.noreply.github.com>
2024-05-07 11:49:47 +0900

    UserController 들여쓰기 정리

--- src/main/kotlin/kr/getx/fitnessteachers/controller/UserController.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/controller/UserController.kt
@@ -14,8 +14,8 @@ import org.springframework.http.ResponseEntity
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
 class UserController(
-  private val userService: UserService,
-)   {
+    private val userService: UserService,
+) {
 
     @GetMapping("/all")
     fun getAllUsers(): ResponseEntity<List<User>> = ResponseEntity.ok(userService.getAllUsers())
@@ -31,21 +31,23 @@ class UserController(
 
     @PostMapping("/login")
     fun loginUser(request: HttpServletRequest): ResponseEntity<Any> {
-      val userDto = request.getAttribute("userData") as? UserDto
-          ?: throw AuthenticationEmailNotFoundException()
+        val userDto = request.getAttribute("userData") as? UserDto
+            ?: throw AuthenticationEmailNotFoundException()
 
-      return try {
-          val user = userService.processUserLogin(userDto)
-          ResponseEntity.ok(user)
-      } catch (e: Exception) {
-        throw UserLoginFailedException("로그인 및 회원가입에 실패했습니다. : ${e.message}")
-      }
+        return try {
+            val user = userService.processUserLogin(userDto)
+            ResponseEntity.ok(user)
+        } catch (e: Exception) {
+            throw UserLoginFailedException("로그인 및 회원가입에 실패했습니다. : ${e.message}")
+        }
     }
 
     @PostMapping("/userTypeEdit")
-    fun userTypeEdit(@RequestBody updateUserRequest: UserDto): ResponseEntity<User> = ResponseEntity.ok(userService.processUserTypeEdit(updateUserRequest))
+    fun userTypeEdit(@RequestBody updateUserRequest: UserDto): ResponseEntity<User> =
+        ResponseEntity.ok(userService.processUserTypeEdit(updateUserRequest))
 
     @PostMapping("/edit/{email}")
-    fun editUser(@PathVariable email: String, @RequestBody updateUserRequest: UserDto): ResponseEntity<User> = ResponseEntity.ok(userService.processUserEdit(email, updateUserRequest))
+    fun editUser(@PathVariable email: String, @RequestBody updateUserRequest: UserDto): ResponseEntity<User> =
+        ResponseEntity.ok(userService.processUserEdit(email, updateUserRequest))
 
 }
