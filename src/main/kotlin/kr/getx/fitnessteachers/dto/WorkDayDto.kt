d6f3f70e81c5ccdd6d8bb9393dde273266b534e0
BigNo219 <103584561+BigNo219@users.noreply.github.com>
2024-06-15 22:01:04 +0900

    [WorkDays] Dto -> JsonCreator 추가

--- src/main/kotlin/kr/getx/fitnessteachers/dto/WorkDayDto.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/dto/WorkDayDto.kt
@@ -1,5 +1,7 @@
 package kr.getx.fitnessteachers.dto
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
 import kr.getx.fitnessteachers.entity.JobPost
 import kr.getx.fitnessteachers.entity.WorkDay
 
@@ -10,6 +12,14 @@ data class WorkDayDto(
     val startTime: String,
     val endTime: String
 ) {
+    @JsonCreator
+    constructor(
+        @JsonProperty("jobPostId") jobPostId: Int,
+        @JsonProperty("day") day: String,
+        @JsonProperty("startTime") startTime: String,
+        @JsonProperty("endTime") endTime: String
+    ) : this(0, jobPostId, day, startTime, endTime)
+
     companion object {
         fun fromEntity(workDay: WorkDay): WorkDayDto = WorkDayDto(
             workDayId = workDay.workDayId,
