c1f82a6fc8698802ee2c9f5ab1445477e72e6d4e
BigNo219 <103584561+BigNo219@users.noreply.github.com>
2024-06-20 15:21:35 +0900

    [WorkDays] JobPostDto Edit

--- src/main/kotlin/kr/getx/fitnessteachers/dto/JobPostDto.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/dto/JobPostDto.kt
@@ -1,5 +1,6 @@
 package kr.getx.fitnessteachers.dto
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import kr.getx.fitnessteachers.entity.Center
 import kr.getx.fitnessteachers.entity.JobPost
 import java.time.LocalDate
@@ -8,45 +9,47 @@ import java.time.LocalDateTime
 data class JobPostDto(
     val jobPostId: Int = 0,
     val center: CenterDto,
-    val isPostCompleted: Boolean = false,
-    val isRecruitmentOpen: Boolean = false,
-    val jobCategories: String = "",
-    val workLocation: String,
-    val workDays: List<WorkDayDto>,
-    val employmentType: String,
-    val salaryType: String,
-    val additionalSalary: String,
-    val minSalary: Int,
-    val maxSalary: Int,
-    val experienceLevel: Int,
-    val numberOfPositions: Int,
+    val isPostCompleted: Boolean? = false,
+    val isRecruitmentOpen: Boolean? = false,
+    val jobCategories: String?,
+    val workLocation: String?,
+    val workDays: List<WorkDayDto> = emptyList(),
+    val employmentType: String?,
+    val salaryType: String?,
+    val additionalSalary: String?,
+    val minSalary: Int? = null,
+    val maxSalary: Int? = null,
+    val experienceLevel: Int?,
+    val numberOfPositions: Int?,
     val qualifications: String?,
     val preferences: String?,
     val details: String?,
+    @JsonFormat(pattern = "yyyy-MM-dd")
     val applicationPeriodEnd: LocalDate? = null,
+    @JsonFormat(pattern = "yyyy-MM-dd")
     val workStartDate: LocalDate?,
-    val isDeleted: Boolean,
+    val isDeleted: Boolean = false,
     val isDeletedAt: LocalDateTime? = null,
-    val title: String,
-    val link: String,
-    var createdAt: LocalDateTime = LocalDateTime.now(),
-    val postedDate: LocalDateTime = LocalDateTime.now()
+    val title: String?,
+    val link: String?,
+    val createdAt: LocalDateTime? = LocalDateTime.now(),
+    val postedDate: LocalDateTime? = LocalDateTime.now()
 ) {
     fun toEntity(center: Center): JobPost {
         val jobPost = JobPost(
             jobPostId = jobPostId,
             center = center,
-            isPostCompleted = isPostCompleted,
-            isRecruitmentOpen = isRecruitmentOpen,
-            jobCategories = jobCategories,
-            workLocation = workLocation,
-            employmentType = employmentType,
-            salaryType = salaryType,
-            additionalSalary = additionalSalary,
-            minSalary = minSalary,
-            maxSalary = maxSalary,
-            experienceLevel = experienceLevel,
-            numberOfPositions = numberOfPositions,
+            isPostCompleted = isPostCompleted ?: false,
+            isRecruitmentOpen = isRecruitmentOpen ?: false,
+            jobCategories = jobCategories ?: "",
+            workLocation = workLocation ?: "",
+            employmentType = employmentType ?: "",
+            salaryType = salaryType ?: "",
+            additionalSalary = additionalSalary ?: "",
+            minSalary = minSalary ?: 0,
+            maxSalary = maxSalary ?: 0,
+            experienceLevel = experienceLevel ?: 0,
+            numberOfPositions = numberOfPositions ?: 0,
             qualifications = qualifications ?: "",
             preferences = preferences ?: "",
             details = details ?: "",
@@ -54,10 +57,10 @@ data class JobPostDto(
             workStartDate = workStartDate,
             isDeleted = isDeleted,
             isDeletedAt = isDeletedAt,
-            title = title,
-            link = link,
-            createdAt = createdAt,
-            postedDate = postedDate
+            title = title ?: "",
+            link = link ?: "",
+            createdAt = createdAt ?: LocalDateTime.now(),
+            postedDate = postedDate ?: LocalDateTime.now()
         )
 
         jobPost.workDays = workDays.map { it.toEntity(jobPost) }
@@ -71,9 +74,9 @@ data class JobPostDto(
             center = CenterDto.fromEntity(jobPost.center),
             isPostCompleted = jobPost.isPostCompleted,
             isRecruitmentOpen = jobPost.isRecruitmentOpen,
-            jobCategories = jobPost.jobCategories ?: "",
-            workLocation = jobPost.workLocation ?: "",
-            workDays = jobPost.workDays.map{ WorkDayDto.fromEntity(it)},
+            jobCategories = jobPost.jobCategories,
+            workLocation = jobPost.workLocation,
+            workDays = jobPost.workDays.map { WorkDayDto.fromEntity(it) },
             employmentType = jobPost.employmentType,
             salaryType = jobPost.salaryType,
             additionalSalary = jobPost.additionalSalary,
@@ -88,9 +91,9 @@ data class JobPostDto(
             workStartDate = jobPost.workStartDate,
             isDeleted = jobPost.isDeleted,
             isDeletedAt = jobPost.isDeletedAt,
-            title = jobPost.title ?: "",
-            link = jobPost.link ?: "",
-            createdAt = jobPost.createdAt ?: LocalDateTime.now(),
+            title = jobPost.title,
+            link = jobPost.link,
+            createdAt = jobPost.createdAt,
             postedDate = jobPost.postedDate
         )
     }
