ac191b6e2fce5be2894a99992dd2daa6768d4f4d
BigNo219 <103584561+BigNo219@users.noreply.github.com>
2024-07-01 14:56:11 +0900

    [Edit] SecurityConfig 수정

--- src/main/kotlin/kr/getx/fitnessteachers/security/SecurityConfig.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/security/SecurityConfig.kt
@@ -9,6 +9,7 @@ import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import kr.getx.fitnessteachers.jwt.JwtTokenFilter
 import kr.getx.fitnessteachers.jwt.JwtUtils
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
@@ -17,27 +18,42 @@ class SecurityConfig {
     val jwtTokenFilter = JwtTokenFilter(JwtUtils())
 
     @Bean
-    fun filterChain(http: HttpSecurity) = http
-        .csrf { it.disable() } // CSRF 보호 기능을 비활성화합니다.
-        .cors { it.configurationSource(corsConfigurationSource()) } // CORS 설정을 적용합니다.
-        .authorizeHttpRequests {
-            it.requestMatchers(*allowedUrls).permitAll() // allowedUrls 배열에 정의된 경로는 인증 없이 접근 가능합니다.
-                .anyRequest().authenticated() // 그 외의 모든 요청은 인증이 필요합니다.
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.csrf { csrf ->
+            csrf.disable()
         }
-        .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED) } // 세션은 필요한 경우에만 생성됩니다.
-        .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter::class.java) // JWT 토큰 필터를 UsernamePasswordAuthenticationFilter 전에 추가합니다.
-        .build()!!
+
+        http.cors { cors ->
+            cors.configurationSource(corsConfigurationSource())
+        }
+
+        http.authorizeHttpRequests { authorize ->
+            authorize
+                .requestMatchers(*allowedUrls).permitAll()
+                .anyRequest().authenticated()
+        }
+
+        http.sessionManagement { session ->
+            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        }
+
+        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter::class.java)
+
+        return http.build()
+    }
 
     @Bean
-    fun corsConfigurationSource() : CorsConfigurationSource {
+    fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
-        configuration.allowedOrigins = listOf("https://www.pilyo.xyz") // 이 출처에서의 요청만 허용합니다.
-        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS") // 이 HTTP 메서드들을 허용합니다.
-        configuration.allowedHeaders = listOf("Authorization", "Content-Type") // 이 헤더들을 허용합니다.
-        configuration.allowCredentials = true // 인증 정보를 포함한 요청을 허용합니다.
-        configuration.maxAge = 3600L // CORS 프리플라이트 요청의 캐시 시간을 1시간으로 설정합니다.
+        configuration.allowedOrigins = listOf("https://www.pilyo.xyz")
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        configuration.allowedHeaders = listOf("Authorization", "Content-Type")
+        configuration.allowCredentials = true
+        configuration.maxAge = 3600L
+
         val source = UrlBasedCorsConfigurationSource()
-        source.registerCorsConfiguration("/**", configuration) // 모든 경로에 대해 이 CORS 설정을 적용합니다.
+        source.registerCorsConfiguration("/**", configuration)
+
         return source
     }
 }
