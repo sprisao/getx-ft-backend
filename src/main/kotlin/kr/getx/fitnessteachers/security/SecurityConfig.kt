3e479ca0ab0f4adef1402734e2de101fb6065935
BigNo219 <103584561+BigNo219@users.noreply.github.com>
2024-07-01 19:05:33 +0900

    [Edit] Prod 버전으로 SecurityConfig 보안 설정 해제

--- src/main/kotlin/kr/getx/fitnessteachers/security/SecurityConfig.kt
+++ src/main/kotlin/kr/getx/fitnessteachers/security/SecurityConfig.kt
@@ -2,25 +2,23 @@ package kr.getx.fitnessteachers.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import kr.getx.fitnessteachers.jwt.JwtTokenFilter
-import kr.getx.fitnessteachers.jwt.JwtUtils
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
-class SecurityConfig {
-    private val allowedUrls = arrayOf("/**")
-
-    private val jwtUtils = JwtUtils()
-    private val jwtTokenFilter = JwtTokenFilter(jwtUtils)
+@EnableWebSecurity
+class SecurityConfig(private val env: Environment) {
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        val isDev = env.activeProfiles.contains("dev")
+
         http.csrf { csrf ->
             csrf.disable()
         }
@@ -29,17 +27,21 @@ class SecurityConfig {
             cors.configurationSource(corsConfigurationSource())
         }
 
-        http.authorizeHttpRequests { authorize ->
-            authorize
-                .requestMatchers(*allowedUrls).permitAll()
-                .anyRequest().authenticated()
+        if (isDev) {
+            http.authorizeHttpRequests { authorize ->
+                authorize.anyRequest().permitAll()
+            }
+        } else {
+            http.authorizeHttpRequests { authorize ->
+                authorize
+                    .requestMatchers("/**").permitAll()
+                    .anyRequest().authenticated()
+            }
         }
 
         http.sessionManagement { session ->
             session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         }
-
-        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter::class.java)
 
         return http.build()
     }
@@ -47,9 +49,9 @@ class SecurityConfig {
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
-        configuration.allowedOrigins = listOf("https://www.pilyo.xyz")
-        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
-        configuration.allowedHeaders = listOf("Authorization", "Content-Type")
+        configuration.allowedOrigins = listOf("*") // 필요한 포트 추가
+        configuration.allowedMethods = listOf("*")
+        configuration.allowedHeaders = listOf("*")
         configuration.allowCredentials = true
         configuration.maxAge = 3600L
 
